404a27073329c46d63146c2ee8aa0093b3bac93a
SultanAli41 <Sultanalibalouch512@gmail.com>
2025-10-29 13:38:22 +0500

    Landing Pages

---
+++ Landing Page 2/index.js
@@ -0,0 +1,47 @@
+let a;
+let b;
+let c;
+let x;
+function abc(){
+x=document.querySelector("#arr");
+x.style.color="black";
+}
+function def(){
+    x=document.querySelector("#arr");
+    x.style.color="rgba(0,0,0,0.31)";
+}
+function abc1(){
+     a=document.querySelector("#line");
+    a.style.borderBottom="2px solid black";
+    a.style.color="black";
+}
+function def1(){
+    a=document.querySelector("#line");
+    a.style.borderBottom="none";
+    a.style.color="rgba(0,0, 0, 0.49)";
+
+}
+function abc2(){
+     b=document.querySelector("#border");
+    b.style.borderBottom="2px solid black";
+    b.style.color="black";
+
+}
+function def2(){
+     b=document.querySelector("#border");
+    b.style.borderBottom="none";
+    b.style.color="rgba(0,0, 0, 0.49)";
+
+}
+function abc3(){
+     c=document.querySelector("#bb");
+    c.style.borderBottom="2px solid black";
+    c.style.color="black";
+
+}
+function def3(){
+     c=document.querySelector("#bb");
+    c.style.borderBottom="none";
+    c.style.color="rgba(0,0, 0, 0.81)";
+
+}
